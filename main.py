5fa863cd5bd3a7ac40de552c831b2e690dadba73
amatt1552 <amatt1552@gmail.com>
2025-08-01 14:53:40 -0400

    Added groups to sort scripts that need to update and a group for objects to be drawn

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
 
     while 0 == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
